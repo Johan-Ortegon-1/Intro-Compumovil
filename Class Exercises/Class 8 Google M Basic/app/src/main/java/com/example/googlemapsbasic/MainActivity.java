fb76ff4eb927c081091bce0fb8fee0f920a31c0d
Johan-Ortegon-1 <johandanielortegonparra@gmail.com>
2020-03-24 23:54:29 -0500

    Basic workshop of Google Maps
    -Ubication
    -Activation of Gestures

---
+++ Class Exercises/Class 8 Google M Basic/app/src/main/java/com/example/googlemapsbasic/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.googlemapsbasic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void activarMapa(View v)
+    {
+        Intent intent = new Intent(MainActivity.this, Mapa.class);
+        startActivity(intent);
+    }
+}
